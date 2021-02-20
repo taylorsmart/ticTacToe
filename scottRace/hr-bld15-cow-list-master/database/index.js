487ede5ff7c65ed26ee83758e697fcc178a786b7
Taylor Smart <taylorsmart@gmail.com>
2021-02-20 13:44:21 -0700

    have queries

--- scottRace/hr-bld15-cow-list-master/database/index.js
+++ scottRace/hr-bld15-cow-list-master/database/index.js
@@ -2,9 +2,8 @@ const mysql = require('mysql');
 
 const connection = mysql.createConnection({
   host: 'localhost',
-  user: 'student',
-  password: 'student',
-  database: 'YOUR_DATABASE_NAME_HERE'
+  user: 'root',
+  database: 'FARM5'
 });
 
 connection.connect((err) => {
@@ -16,6 +15,26 @@ connection.connect((err) => {
 });
 
 // Your Database Queries Here!!
+
+const fetchCows =function (cb) {
+  connection.query('SELECT * from cows', (err,cowList) =>{
+    if(err) {
+      console.log(err)
+    } else {
+      cb(cowList)
+    }
+  })
+}
+
+const insertCow =function (name, description, cb) {
+  connection.query(`insert cows columns (name,description) values ('${name}','${description}')`, (err,cowList) =>{
+    if(err) {
+      console.log(err)
+    } else {
+      console.log('COW INSERTED')
+    }
+  })
+}
 
 
 
